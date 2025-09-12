59c460f504bfe0f05a83b36befee4b48e70c20cc
Sebastian Rykowski Zeerak <sebastian.zeerak@gmail.com>
2025-09-12 14:08:12 +0200

    fix: add linter for json

--- init.lua
+++ init.lua
@@ -658,6 +658,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'ruff' },
+        json = { 'prettierd', 'prettier', 'jq' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
