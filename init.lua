ef45401fe308a5c0b914972b6ce49782ef565e0e
Sebastian Rykowski Zeerak <sebastian.zeerak@gmail.com>
2026-02-21 17:25:50 +0100

    feature(php): add php lsp

--- init.lua
+++ init.lua
@@ -410,6 +410,15 @@ require('lazy').setup({
           filetypes = { 'swift', 'objective-c', 'objective-cpp' },
           root_dir = require('lspconfig.util').root_pattern('Package.swift', '.git'),
         },
+        intelephense = {
+          settings = {
+            intelephense = {
+              files = {
+                maxSize = 1000000,
+              },
+            },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
@@ -478,7 +487,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, py = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -491,7 +500,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
+        -- python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
         json = { 'prettierd', 'prettier', 'jq' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
